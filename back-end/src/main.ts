c76aa21d7e21b45b7bd914364b96c50ebd99429d
Kauan Vidigal <kauan.vidigal@gmail.com>
2025-02-26 15:59:52 -0300

    Creating the mongodb database module

---
+++ back-end/src/main.ts
@@ -0,0 +1,59 @@
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import helmet from 'helmet';
+import * as https from 'https';
+import * as fs from 'fs';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const isHttps = process.env.HTTPS === 'true';
+const PORT = process.env.PORT || 3000;
+const API_FRONTEND = process.env.API_FRONTEND || 'http://localhost:5173';
+
+
+async function bootstrap() {
+
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: API_FRONTEND,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type, Accept, Authorization',
+    });
+
+    app.use(helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                scriptSrc: ["'self'", "'unsafe-inline'"],
+                objectSrc: ["'none'"],
+                upgradeInsecureRequests: [],
+            },
+        },
+        frameguard: {action: 'deny'},
+        hidePoweredBy: true,
+        hsts: {maxAge: 31536000, includeSubDomains: true},
+        ieNoOpen: true,
+        noSniff: true,
+        xssFilter: true,
+    }));
+
+    if (isHttps) {
+        const httpsOptions = {
+            key: fs.readFileSync('path/to/your/private-key.pem'),
+            cert: fs.readFileSync('path/to/your/certificate.pem'),
+        };
+        https.createServer(httpsOptions, app.getHttpAdapter().getInstance()).listen(PORT, () => {
+            console.log(`\n\u001b[34mHTTPS server running on https://localhost:${PORT} -> by https://github.com/Vidigal-code\u001b[39m`);
+        });
+    } else {
+        await app.listen(PORT, () => {
+            //app.enableCors();
+            console.log(`\n\u001b[34mHTTP server running on http://localhost:${PORT} -> by https://github.com/Vidigal-code\u001b[39m`);
+        });
+    }
+}
+
+bootstrap();
