c25c4b1671a9c7813eb8bcd70edce6fcd19a8d2d
Kauan Vidigal <kauan.vidigal@gmail.com>
2025-03-14 03:24:52 -0300

    jest config front-end

--- front-end/jest.config.js.ts
+++ front-end/jest.config.js.ts
@@ -1,10 +1,13 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'jsdom',
     moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'node'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
     setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
     testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/'],
+    testMatch: [
+        '**/test/**/*.test.ts',
+        '**/test/**/*.spec.ts'
+    ],
 };
