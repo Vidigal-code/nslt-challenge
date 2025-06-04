22db5913fd257a5f3f112484f2f1918e7cd1d869
Vidigal-code <kauan.vidigal@gmail.com>
2025-06-04 17:57:51 -0300

    fix entrypoint.sh setup docker

--- order-notification-lambda/entrypoint.sh
+++ order-notification-lambda/entrypoint.sh
@@ -6,15 +6,12 @@ aws configure set aws_access_key_id test
 aws configure set aws_secret_access_key test
 aws configure set region us-east-1
 
-# LocalStack runs inside the Docker container, simulating AWS services locally.
-# This allows AWS CLI commands to interact with these services without accessing real AWS.
 echo "[3/6] Creating SNS topic on LocalStack..."
 aws --endpoint-url=http://localstack:4566 sns create-topic --name test-sns-topic
 aws --endpoint-url=http://localstack:4566 sns list-topics
 
+echo "[4/6] Installing serverless-dotenv-plugin and other dependencies..."
 npm install serverless-dotenv-plugin --save-dev --force
-
-echo "[4/6] Installing npm dependencies..."
 npm install --force
 
 echo "[5/6] Starting Serverless Offline..."
